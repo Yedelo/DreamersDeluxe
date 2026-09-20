4299c696000a924926d744f21bdf7ce41c3917a9
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-09-20 12:01:15 -0400

    Implement replace team names with verity variants for modern

---
+++ src/main/java/at/yedel/dreamersdeluxe/mixins/modern/ScoreboardRenderingMixin.java
@@ -0,0 +1,28 @@
+//? if modern {
+package at.yedel.dreamersdeluxe.mixins.modern;
+
+
+import at.yedel.dreamersdeluxe.config.DreamersConfig;
+ import at.yedel.dreamersdeluxe.utils.ServerLocation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+    //~ if < 26.2 'Hud' -> 'Gui'
+import net.minecraft.client.gui.Gui;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+
+
+//~ if < 26.2 'Hud' -> 'Gui'
+@Mixin(Gui.class)
+public abstract class ScoreboardRenderingMixin {
+    //~ if < 26.2 'Hud' -> 'Gui'
+    @ModifyExpressionValue(method = "displayScoreboardSidebar", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/Gui$1DisplayEntry;name:Lnet/minecraft/network/chat/Component;"))
+    private Component dreamersdeluxe$replaceTeamNamesWithVerityVariants(Component original) {
+        if (DreamersConfig.getInstance().enabled && DreamersConfig.getInstance().replaceTeamNamesWithVerityVariants && ServerLocation.getInstance().isInBedwars()) {
+            return Component.literal(DreamersConfig.getInstance().getVerityText(original.getString()));
+        }
+        return original;
+    }
+}
+//?}
