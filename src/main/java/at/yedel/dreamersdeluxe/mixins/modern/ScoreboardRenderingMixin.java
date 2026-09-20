62c6be67f571f69e6351ae322bd088655e1be014
Yedelo <158670400+Yedelo@users.noreply.github.com>
2026-09-20 12:10:55 -0400

    Switch vcs version to 26.2

--- src/main/java/at/yedel/dreamersdeluxe/mixins/modern/ScoreboardRenderingMixin.java
+++ src/main/java/at/yedel/dreamersdeluxe/mixins/modern/ScoreboardRenderingMixin.java
@@ -6,7 +6,7 @@ import at.yedel.dreamersdeluxe.config.DreamersConfig;
  import at.yedel.dreamersdeluxe.utils.ServerLocation;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
     //~ if < 26.2 'Hud' -> 'Gui'
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.Hud;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.At;
 
 
 //~ if < 26.2 'Hud' -> 'Gui'
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public abstract class ScoreboardRenderingMixin {
     //~ if < 26.2 'Hud' -> 'Gui'
-    @ModifyExpressionValue(method = "displayScoreboardSidebar", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/Gui$1DisplayEntry;name:Lnet/minecraft/network/chat/Component;"))
+    @ModifyExpressionValue(method = "displayScoreboardSidebar", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/Hud$1DisplayEntry;name:Lnet/minecraft/network/chat/Component;"))
     private Component dreamersdeluxe$replaceTeamNamesWithVerityVariants(Component original) {
         if (DreamersConfig.getInstance().enabled && DreamersConfig.getInstance().replaceTeamNamesWithVerityVariants && ServerLocation.getInstance().isInBedwars()) {
             return Component.literal(DreamersConfig.getInstance().getVerityText(original.getString()));
